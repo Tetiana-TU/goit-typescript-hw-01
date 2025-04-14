995e7f3228eb921ce4b4309ff7fba12cc7e9bbb9
Tetyana_Dmytryienko <dmitrienko_tanya@ukr.net>
2025-04-14 20:32:15 +0300

    ts hw1 n1

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+import { age, username, toggle, empty, callback } from  './basic/1'
+console.log('age:', age);
+console.log('Username:', username);
+console.log(toggle);
+console.log(empty);
+console.log(callback(10));
