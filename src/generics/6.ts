ebda1fc6c494abee72149d560bf105fafc507125
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2025-04-16 16:12:21 +0300

    correct error

--- src/generics/6.ts
+++ src/generics/6.ts
@@ -17,3 +17,13 @@ type Form = {
 // щоб унеможливити поле 'errors' з типу Form
 
 type Params = Omit<Form, "errors">;
+function submitForm(data: Params) {
+  console.log("Submitting form with data:", data);
+}
+const formData: Params = {
+  email: "test@example.com",
+  firstName: "Tanya",
+  lastName: "Dmytyienko",
+  phone: "1234567890",
+};
+submitForm(formData);
